7a336c42652c0cda55dd67f7e885c49b31863a1b
Khris Kempis <khriskempis@gmail.com>
2019-01-23 14:33:03 -0800

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const header = () => {
+  return (
+    <header className="App-header">
+      <h3>Oh My Goods</h3>
+    </header>
+  )
+}
+
+export default header
---
+++ src/pages/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Header from '../components/header'
+
+import '../styles/main.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+
+        
+
+      
+        
+      </div>
+    );
+  }
+}
+
+export default App;
